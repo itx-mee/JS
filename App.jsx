19584786d7aa350dfb5d605620ad696051137a89
itx-mee <141204585+itx-mee@users.noreply.github.com>
2024-04-20 09:46:11 +0530

    added one file app.jsx

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Card from "./Card component.jsx";
+
+function App() {
+  return (
+    <>
+      <h1> Tailwind CSS! </h1>
+      <Card />
+    </>
+  );
+}
+
+export default App;
